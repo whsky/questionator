376ec848541c7d694267d0d585a52d7cf01450b3
Steve Iannaccone <renegadeoffunk1200@yahoo.com>
2017-04-11 12:38:07 -0600

    v2.0 with better random selections.

--- questionator.py
+++ questionator.py
@@ -7,7 +7,8 @@
 from flask import Flask, request, url_for, render_template, request
 import cPickle as pickle
 import numpy as np
-from random import randrange
+import pandas as pd
+from random import randrange, sample, choice
 
 app = Flask(__name__)
 
@@ -19,18 +20,36 @@ def index():
 
 @app.route('/question', methods=['POST'])
 def pnt_predicter():
-    random_index = randrange(0,len(roster))
+    df_min = df[df.num_quest == min(df.num_quest)]
+    stud_min = df_min.index.values.tolist()
+    #times_asked = df.num_quest[sub_pop]
+    random_index = choice(stud_min)
 
     avatar_link = avatar[random_index]
     member_name = roster[random_index]
 
+    df.num_quest[random_index] += 1
+
     return render_template('question.html',
                             avatar=avatar_link,
-                            name=member_name)
+                            name=member_name,
+                            quest=df.num_quest[random_index])
 
 
 
 if __name__ == '__main__':
     roster = pickle.load(open('data/roster', 'rb'))
     avatar = pickle.load(open('data/avatar', 'rb'))
+    df = pd.DataFrame()
+    df['roster'] = roster
+    df['avatar'] = avatar
+    df['num_quest'] = [0]*len(roster)
     app.run(host='0.0.0.0', port=8080, threaded=True)
+
+
+#create a file tracking the number of times a student has popped up on
+# Questionator so we can weight a distribution to make them less likely
+# to be called on next time.
+
+#with open('filename', 'w'):
+#   num_questions_answ[random_index] += 1
